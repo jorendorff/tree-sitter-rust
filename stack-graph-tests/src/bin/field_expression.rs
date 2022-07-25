4a931c44411c43825597b5700c2b89bb1a59f8cd
Jason Orendorff <jason.orendorff@gmail.com>
2022-07-25 18:55:28 -0500

    Set the type of some blocks.

--- stack-graph-tests/src/bin/field_expression.rs
+++ stack-graph-tests/src/bin/field_expression.rs
@@ -27,6 +27,45 @@ pub fn card_name_by_ref(r: &ReportCard) -> &str {
     //         ^ defined: 2
 }
 
+pub fn if_expr_type(select_roman: bool) -> String {
+    let student = if select_roman {
+        Student { name: "roman".to_string() }
+    } else {
+        Student { name: "ram".to_string() }
+    };
+    student.name
+    //      ^ defined: 2
+}
+
+pub fn block_type(select_roman: bool) -> String {
+    let student1 = {
+        Student { name: "ram".to_string() }
+    };
+    let _ = &student1.name;
+    //                ^ defined: 2
+
+    // Blocks where the final expression ends with a block are tricky.
+    // Doable, but for a bug in the tree-sitter query engine:
+    // <https://github.com/tree-sitter/tree-sitter/issues/1811>
+    let roman = {
+        {
+            Student { name: "roman".to_string() }
+        }
+    };
+    let _ = &roman.name;
+    //             ^ UNSUPPORTED
+
+    let student = {
+        if select_roman {
+            roman
+        } else {
+            student1
+        }
+    };
+    student.name
+    //      ^ UNSUPPORTED
+}
+
 fn main() {
     let kaitlin = Student { name: "kaitlin".to_string() };
     let _ = kaitlin.name;
